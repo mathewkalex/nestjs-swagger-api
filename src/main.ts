a90064fee8139f6e570a36cb823a50ca2d76fb0c
Mathew K Alex <mathewkalex.mec@gmail.com>
2022-08-08 17:56:46 +0530

    first commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder().setTitle('Demo Application')
+                      .setDescription("Demo API Application")
+                      .setVersion('v1')
+                      .addTag('notes')
+                      .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  await app.listen(3000);
+}
+bootstrap();
